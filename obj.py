04b36d77a91f21708afdc3067b6c4993173593ba
SwayamChandak <chandakswayam3904@gmail.com>
2025-06-24 16:05:00 +0530

    this pushing to main branch

--- obj.py
+++ obj.py
@@ -4,12 +4,12 @@ class Animal:
     
     def speak(self, sound):
         print(f"{self.name} will make some noise like {sound}")
-class Dog(Animal):
-    def speak(self):
-        print(f"{self.name} will bark")
+# class Dog(Animal):
+#     def speak(self):
+#         print(f"{self.name} will bark")
     
 name=input("input name of your animal ")
 sound=input(f"input sound that {name} makes ")
-anim=Dog("rex")
-anim.speak()
+# anim=Dog("rex")
+# anim.speak()
 Animal(name).speak(sound)
